b150d39470a276ca31f84805acaa50aeca58ba85
Sri Manikanta Palakollu <srimani.scholar@gmail.com>
2021-07-27 13:04:19 +0530

    Arrow function features.

--- ES6 Features/arrow-functions.js
+++ ES6 Features/arrow-functions.js
@@ -1,3 +1,9 @@
+const generalSquare = function (x) {
+  return x * x;
+};
+
+console.log(generalSquare(7));
+
 const square = (x) => {
   return x * x;
 };
@@ -7,3 +13,40 @@ console.log("Square of a number: ", square(5));
 const cube = (x) => x * x * x;
 
 console.log("Cube of a number: ", cube(3));
+
+// arguments object is no longer bound to the arrow functions
+
+const generalAddAll = function () {
+  let sum = 0;
+
+  for (let i = 0; i < arguments.length; i++) {
+    if (typeof arguments[i] === "number") {
+      sum += arguments[i];
+    }
+  }
+
+  return sum;
+};
+
+console.log(
+  "Total Add values are:",
+  generalAddAll(1, 2, 3, 4, 5, "a", "hello", 7)
+);
+
+// The below code will throw an error because this keyword is no lnger bound to the arrow functions
+// const arrowAddAll = () => {
+//   let sum = 0;
+
+//   for (let i = 0; i < arguments.length; i++) {
+//     if (typeof arguments[i] === "number") {
+//       sum += arguments[i];
+//     }
+//   }
+
+//   return sum;
+// };
+
+// console.log(
+//   "Total Add values are:",
+//   arrowAddAll(1, 2, 3, 4, 5, "a", "hello", 7)
+// );
